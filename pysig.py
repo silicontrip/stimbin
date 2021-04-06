3ed79a1bc83108e62d0cdcb9d170e22cb3c2d367
mark heath (@somnio) <silicontrip@gmail.com>
2021-04-06 21:57:48 +1000

    Added Scale, Log and Random generators.

--- pysig.py
+++ pysig.py
@@ -1,5 +1,6 @@
 import math
 import numpy
+import numpy.random
 import scipy.io.wavfile
 import scipy.signal
 import scipy.integrate
@@ -7,12 +8,19 @@ import scipy.integrate
 import matplotlib.pyplot as pt
 
 def DB(db):
-	return 10 ** (db / 20.0)	
+	return 10 ** (db / 20.0)
+
+def Degrees(deg):
+	return deg / 180 * math.pi
 
 def write(signalleft,right=None,sps=44100,name="out.wav"):
 	dur = signalleft.duration()
+	if (math.isinf(dur)):
+		raise Exception("No Duration defined")
+	
 	print("Duration:" + str(dur) + " sps: " + str(sps) )
-	sample=numpy.arange(0,dur,1/sps)
+#	sample=numpy.arange(0,dur,1/sps)
+	sample=numpy.linspace(0,dur,num=sps * dur)
 	sleft = signalleft.getsample(sample)
 	if right is not None:
 		sright = right.getsample(sample)
@@ -23,20 +31,23 @@ def write(signalleft,right=None,sps=44100,name="out.wav"):
 		scipy.io.wavfile.write(name, sps, sleft.astype(numpy.int16))
 
 def writestereo(sigl,sigr,sps,name):
-	dur = sigl.duration()
-	sample=numpy.arange(0,dur,1/sps)
-	left = sigl.getsample(sample)
-	right = sigr.getsample(sample)
+	left = sigl.getnd(sps)
+	right = sigr.getnd(sps)
 	out = numpy.array([left,right])
 
 	scipy.io.wavfile.write(name, sps, numpy.transpose(out))
 
 def writemono(sigl,sps,name):
-	dur = sigl.duration()
-	sample=numpy.arange(0,dur,1/sps)
-	left = sigl.getsample(sample)
+	scipy.io.wavfile.write(name, sps, sigl.getnd(sps))
 
-	scipy.io.wavfile.write(name, sps, left)
+def plotsig(sigl,samples=1000,dur=None):
+	if (dur is None):
+		dur = sigl.duration()
+	spl = numpy.arange(0,dur,dur/samples)
+
+	pt.plot(spl,sigl.getsample(spl))	
+	pt.show()
+
 
 def signal(sig):
 	if isinstance(sig,SignalGenerator):
@@ -60,11 +71,14 @@ class SignalGenerator:
 		return self.getsample(sample)
 
 	def write(self,sps,name):
-		dur = self.duration()
-		sample=numpy.arange(0,dur,1/sps)
-		wavout = self.getsample(sample)
-
+		wavout = self.getnd(sps)
 		scipy.io.wavfile.write(name, sps, wavout)
+
+	def getrange(self,index=None):
+		if (index is None):
+			return [-math.inf,math.inf]
+		else:
+			return [-math.inf,math.inf][index]
 
 # converts -1 to 1 to 0 to 1. IE standard sine output
 class Unitise(SignalGenerator):
@@ -81,6 +95,71 @@ class Unitise(SignalGenerator):
 	def duration(self):
 		return self.signal.duration()
 
+	def getrange(self,index=None):
+		if (index is None):
+			return [0,1]
+		else:
+			return [0,1][index]
+
+class Scale(SignalGenerator):
+
+	signal=SignalGenerator()
+#	offset=-0.5
+	scale=0.5
+	newmin=0
+	oldmin=-1
+	range =[]
+
+	def __init__(self,sig,inscale=[-1,1],outscale=[0,1]):
+		# probably not needed... why would you unitise a DC signa?
+		self.signal = signal(sig)
+		self.newmin = signal(outscale[0])
+		self.oldmin = signal(inscale[0])
+
+		in0 = signal(inscale[0])
+	#	plotsig(in0,dur=10)
+		in1 = signal(inscale[1])
+		#plotsig(in1,dur=10)
+
+		out0 = signal(outscale[0])
+		#plotsig(out0,dur=10)
+
+		out1 = signal(outscale[1])
+		#plotsig(out1,dur=10)
+
+		intime = Add(in1, Negative(in0))
+		#plotsig(intime,dur=10)
+
+		outtime = Add(out1, Negative(out0))
+		#plotsig(outtime,dur=10)
+
+		self.scale = Div(outtime,intime)
+		#plotsig(self.scale,dur=10)
+
+	#	diff = Add(out0,Negative(in0))
+		#plotsig(diff,dur=10)
+
+	#	self.offset = AM(diff , self.scale)
+	#	plotsig(self.offset,dur=10)
+
+		#self.scale = (numpy.subtract(signal(outscale[1]), signal(outscale[0])) / (signal(inscale[1])-signal(inscale[0])))
+		#self.offset = signal(outscale[0]) - (signal(inscale[0]) * self.scale)
+
+		self.range = [min(out1.getrange(0),out0.getrange(0)), max(out1.getrange(1),out0.getrange(1))]
+		#print(self.range)
+
+	def getsample(self,time):
+		return ( ( self.signal.getsample(time) - self.oldmin.getsample(time)) * self.scale.getsample(time) ) + self.newmin.getsample(time)
+
+	def duration(self):
+		return self.signal.duration()
+
+	def getrange(self,index=None):
+		if (index is None):
+			return self.range
+		else:
+			return self.range[index]
+
 class AbsSig(SignalGenerator):
 
 	signal=SignalGenerator()
@@ -94,6 +173,12 @@ class AbsSig(SignalGenerator):
 	def duration(self):
 		return self.signal.duration()
 
+	def getrange(self,index=None):
+		if (index is None):
+			return [0,self.signal.getrange(index=1)]
+		else:
+			return [0,self.signal.getrange(index=1)][index]
+
 class Negative(SignalGenerator):
 
 	signal=SignalGenerator()
@@ -106,6 +191,12 @@ class Negative(SignalGenerator):
 
 	def duration(self):
 		return self.signal.duration()
+
+	def getrange(self,index=None):
+		if (index is None):
+			return [0.0-self.signal.getrange(index=0),0.0-self.signal.getrange(index=1)]
+		else:
+			return 0.0-self.signal.getrange(index=index)
 
 class DC(SignalGenerator):
 	y=0
@@ -122,6 +213,31 @@ class DC(SignalGenerator):
 	def duration(self):
 		return self.dur
 
+	def getrange(self,index=None):
+		if (index is None):
+			return [self.y,self.y]
+		else:
+			return self.y
+
+class Log(SignalGenerator):
+
+	signal=SignalGenerator()
+
+	def __init__(self,sig):
+		self.signal = signal(sig)
+
+	def getsample(self,time):
+		return numpy.log10(self.signal.getsample(time))
+
+	def duration(self):
+		return self.signal.duration()
+
+	def getrange(self,index=None):
+		if (index is None):
+			return [0,numpy.log10(self.signal.getrange(index=1))]
+		else:
+			return [0,self.signal.getrange(index=1)][index]
+
 class Linear(SignalGenerator):
 
 	envelope_time = [0,1]
@@ -136,6 +252,14 @@ class Linear(SignalGenerator):
 
 	def duration(self):
 		return max(self.envelope_time)
+
+	def getrange(self,index=None):
+		if (index is None):
+			return [min(self.envelope_amplitude),max(self.envelope_amplitude)]
+		else:
+			return [min(self.envelope_amplitude),max(self.envelope_amplitude)][index]
+
+
 
 class Sine(SignalGenerator):
 	amplitude = 1
@@ -192,13 +316,48 @@ class Sine(SignalGenerator):
 	def duration(self):
 		return min([self.hertz.duration(), self.phase.duration(), self.amplitude.duration(), self.dur])
 
+	def getrange(self,index=None):
+		# arg amplitude
+		if (index is None):
+			return self.amplitude.getrange()
+		else:
+			return self.amplitude.getrange(index=index)
+
+class Random(Sine):
+	amplitude=1
+	hertz=0
+	phase=0
+
+	rng=None
+	
+	def __init__(self,seed,amplitude=None,duration=None):
+		self.rng = numpy.random.RandomState(seed)
+		self.hertz=DC(0)
+		self.phase=DC(0)
+		# print(self.rng)
+
+		if amplitude is None:
+			self.amplitude = DC(1)  # 1 = DB(0)
+		else:
+			self.amplitude = signal(amplitude)
+
+		if duration is None:
+			self.dur = math.inf
+		else:
+			self.dur = duration
+
+	def getsample(self,time):
+		#print(time.shape)
+		return self.amplitude.getsample(time) * self.rng.random_sample(size=time.shape)
+
+
 class Bipolar(Sine):
 	amplitude = 0
 	hertz = 0
 	phase = 0
 	dur=0
-	pduty=0.5
-	nduty=0.5
+	pduty=DC(0.5)
+	nduty=DC(0.5)
 	duty=0.5
 
 	def __init__(self,h,amplitude=None,phase=None,duty=None,duration=None):
@@ -217,6 +376,7 @@ class Bipolar(Sine):
 		if duty is None:
 			self.pduty = DC(0.5)
 			self.nduty = DC(0.5)
+			self.duty = DC(0.5)
 		else:
 			self.duty = signal(duty)
 
@@ -255,6 +415,7 @@ class Bipolar(Sine):
 
 		return ama * numpy.round(numpy.sin(timea))
 
+
 class Sawtooth(Sine):
 	amplitude = 0
 	hertz = 0
@@ -267,11 +428,10 @@ class Sawtooth(Sine):
 
 		timea = scipy.integrate.cumtrapz(hza,time,initial=0)
 
-
 		timea = timea * 2 * math.pi
 		timea = timea + pha
 		# need to do pwm
-		return self.amplitude.getsample(timea) * numpy.interp(timea,[-math.pi,math.pi],[-1,1],period=2*math.pi)
+		return self.amplitude.getsample(timea) * numpy.interp(timea,[0,2*numpy.pi],[1,-1],period=2*numpy.pi)
 
 class Square(Sine):
 	amplitude = 0
@@ -331,6 +491,7 @@ class Square(Sine):
 
 		return ama * scipy.signal.square(timea,dta)
 
+# same as Multiply
 class AM(SignalGenerator):
 	sig1 = {}
 	sig2 = {}
@@ -346,6 +507,24 @@ class AM(SignalGenerator):
 		dur1 = self.sig1.duration()
 		dur2 = self.sig2.duration()
 		return min([dur1,dur2])
+
+	def getrange(self,index=None):
+		if (index is None):
+			return [self.sig1.getrange(index=0)*self.sig2.getrange(index=0),self.sig1.getrange(index=1)*self.sig2.getrange(index=1)]
+		else:
+			return [self.sig1.getrange(index=0)*self.sig2.getrange(index=0),self.sig1.getrange(index=1)*self.sig2.getrange(index=1)][index]
+
+class DBS(SignalGenerator):
+	sig={}
+
+	def __init__(self,a):
+		self.sig=signal(a)
+
+	def getsample(self,time):
+		return 10 ** (self.sig.getsample(time) / 20.0)
+
+	def duration(self):
+		return self.sig.duration()
 
 class Div(SignalGenerator):
 	sig1 = {}
@@ -363,6 +542,49 @@ class Div(SignalGenerator):
 		dur2 = self.sig2.duration()
 		return min([dur1,dur2])
 
+def getrange(self,index=None):
+		if (index is None):
+			return [self.sig1.getrange(index=0)/self.sig2.getrange(index=0),self.sig1.getrange(index=1)/self.sig2.getrange(index=1)]
+		else:
+			return [self.sig1.getrange(index=0)/self.sig2.getrange(index=0),self.sig1.getrange(index=1)/self.sig2.getrange(index=1)][index]
+
+class Hilbert(SignalGenerator):
+	signal=SignalGenerator()
+
+	def __init__(self,sig):
+		self.signal = signal(sig)
+
+	def getsample(self,time):
+		return numpy.imag(scipy.signal.hilbert(self.signal.getsample(time)))
+
+	def duration(self):
+		return self.signal.duration()
+
+	def getrange(self,index=None):
+		if (index is None):
+			return [0,numpy.imag(scipy.signal.hilbert(self.signal.getrange(index=1)))]
+		else:
+			return [0,self.signal.getrange(index=1)][index]	
+
+class Gradient(SignalGenerator):
+	signal=SignalGenerator()
+
+	def __init__(self,sig):
+		self.signal = signal(sig)
+
+	def getsample(self,time):
+		return numpy.gradient(self.signal.getsample(time))
+
+	def duration(self):
+		return self.signal.duration()
+
+	def getrange(self,index=None):
+		if (index is None):
+			return [0,numpy.gradient(self.signal.getrange(index=1))]
+		else:
+			return [0,self.signal.getrange(index=1)][index]	
+
+
 class Add(SignalGenerator):
 	sig1 = {}
 	sig2 = {}
@@ -378,6 +600,12 @@ class Add(SignalGenerator):
 		dur1 = self.sig1.duration()
 		dur2 = self.sig2.duration()
 		return min([dur1,dur2])
+
+	def getrange(self,index=None):
+		if (index is None):
+			return [self.sig1.getrange(index=0)+self.sig2.getrange(index=0),self.sig1.getrange(index=1)+self.sig2.getrange(index=1)]
+		else:
+			return [self.sig1.getrange(index=0)+self.sig2.getrange(index=0),self.sig1.getrange(index=1)+self.sig2.getrange(index=1)][index]
 
 class Cat(SignalGenerator):
 	sig1 = {}
@@ -395,6 +623,12 @@ class Cat(SignalGenerator):
 		dur1 = self.sig1.duration()
 		dur2 = self.sig2.duration()
 		return dur1+dur2
+
+	def getrange(self,index=None):
+		if (index is None):
+			return [min(self.sig1.getrange(index=0),self.sig2.getrange(index=0)),max(self.sig1.getrange(index=1),self.sig2.getrange(index=1))]
+		else:
+			return [min(self.sig1.getrange(index=0),self.sig2.getrange(index=0)),max(self.sig1.getrange(index=1),self.sig2.getrange(index=1))][index]
 
 class Mplex(SignalGenerator):
 	sigm={}
@@ -414,6 +648,12 @@ class Mplex(SignalGenerator):
 	def duration(self):
 		return self.sigm.duration()	
 
+	def getrange(self,index=None):
+		if (index is None):
+			return [min(self.sig1.getrange(index=0),self.sig2.getrange(index=0)),max(self.sig1.getrange(index=1),self.sig2.getrange(index=1))]
+		else:
+			return [min(self.sig1.getrange(index=0),self.sig2.getrange(index=0)),max(self.sig1.getrange(index=1),self.sig2.getrange(index=1))][index]
+
 class Wave(SignalGenerator):
 	samples = 0
 	sps = 1
@@ -424,15 +664,17 @@ class Wave(SignalGenerator):
 		self.sps,aud = scipy.io.wavfile.read(name)
 
 		self.samples=aud.shape
+		#print(self.samples)
 		self.periodic=periodic
 
 		chan = 1
-		if (len(self.samples)>1):
-			(self.samples,chan) = self.samples
+		
+		(self.samples,) = self.samples
 
 		# override sample rate to get desired duration.
 		if duration is not None:
-			self.sps = duration * samples
+			#print("Setting sps = ", self.samples / duration) 
+			self.sps =  self.samples / duration
 
 		caud = aud.transpose()
 
@@ -458,7 +700,8 @@ class Wave(SignalGenerator):
 		#print(self.audio.shape)
 
 		#return self.audio[spos]
-		return numpy.where (spos<=self.samples,self.audio[spos],0)
+		#print( "spos " , spos)
+		return self.audio[numpy.where ((spos>=0) & (spos<self.samples), spos,0)]
 		#return numpy.piecewise(spos,[spos<=self.samples],[lambda spos: self.audio[spos],0])
 
 #class Dur(SignalGenerator):
