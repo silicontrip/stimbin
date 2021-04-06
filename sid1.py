76040236a511ffbc3d8e40a957a41c4c696f7f68
mark heath (@somnio) <silicontrip@gmail.com>
2021-04-06 22:19:14 +1000

    catching up on all the files I didn't commit.

--- sid1.py
+++ sid1.py
@@ -8,9 +8,6 @@ dp=ps.Linear([0,1],[0,2*numpy.pi])
 di=ps.Linear([0,1],[0,10])
 carrier = ps.Sine(50,distortion=1.1,distortionphase=dp,duration=1)
 
+ps.plotsig(carrier,5120)
 
 
-rng = numpy.linspace(0,1,5120)
-
-pt.plot(carrier.getsample(rng))
-pt.show()
